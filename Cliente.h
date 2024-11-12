47a4056bfaaf83f24782fc52e281b163bb08592d
Davi <davicardos@ufrrj.br>
2024-11-12 15:09:56 -0300

    first commit

--- Cliente.h
+++ Cliente.h
@@ -7,17 +7,13 @@
 typedef struct Cliente {
     int codCliente;
     char nome[50];
-    char data_nascimento[20];
 } Cliente;
 
 // Imprime cliente
 void imprime(Cliente* func);
 
 // Cria cliente. Lembrar de usar free(cliente)
-Cliente* cliente(int cod, char* nome, char* data_nascimento);
-
-// Salva cliente no arquivo out, na posicao atual do cursor
-void salva(Cliente* func, FILE* out);
+Cliente* cliente(int cod, char* nome);
 
 // Le um cliente do arquivo in na posicao atual do cursor
 // Retorna um ponteiro para cliente lido do arquivo
--- TableHash.c
+++ TableHash.c
@@ -7,3 +7,26 @@ typedef struct {
     char Nome[100];
 }CLIENTE;
 
+typedef struct {
+    CLIENTE *cliente;
+    struct ENDERECO *prox;
+} ENDERECO;
+
+typedef struct {
+ENDERECO **tabela; 
+int tamanhoAtual; 
+int fatorCarga; 
+int duplicacoes; 
+int proximoCompartimento; 
+} TABELAHASH;
+
+TABELAHASH* inicializarTABELAHASH(int tamanhoInicial, int fatorCarga) { 
+    
+TABELAHASH *tabela = (TABELAHASH*)malloc(sizeof(TABELAHASH)); 
+tabela->tabela = (ENDERECO**)calloc(tamanhoInicial, sizeof(ENDERECO*)); 
+tabela->tamanhoAtual = tamanhoInicial; 
+tabela->fatorCarga = fatorCarga; 
+tabela->duplicacoes = 0; 
+tabela->proximoCompartimento = 0; 
+return tabela;
+}
